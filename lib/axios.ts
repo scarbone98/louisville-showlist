7acf2dba828e5ecfd7723f753524c41f5af8c5e8
Samuel Carbone <scarbone1998@gmail.com>
2024-04-28 15:24:30 -0400

    first vercel env var

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
-const baseURL = process.env.BASE_URL || 'http://127.0.0.1:3000';
+const baseURL = process.env.VERCEL_URL || 'http://127.0.0.1:3000';
 
 const axiosInstance = axios.create({
     baseURL,
